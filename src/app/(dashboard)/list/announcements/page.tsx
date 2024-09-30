422438ae02c47a7dac4b6d452c0e38ef865ce70c
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-09-30 22:18:51 +0300

    fetched data to announcements list page and set filter for title and class

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -3,6 +3,9 @@ import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
 import { announcementsData, role } from '@/lib/data'
+import prisma from '@/lib/prisma'
+import { ITEMS_PER_PAGE } from '@/lib/settings'
+import { Announcement, Class, Prisma } from '@prisma/client'
 import Image from 'next/image'
 import React from 'react'
 
@@ -26,49 +29,81 @@ const columns = [
         accessor: "actions",
     },
 ]
-export type Announcement = {
-    id: number;
-    title: string;
-    class: string;
-    date: string
-}
-const AnnouncementsListPage = () => {
-    const renderRow = (item: Announcement) => (
-        <tr
-            key={item.id}
-            className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-mPurpleLight'
-        >
-            <td className='flex items-center gap-4 p-4'>
+export type AnnouncementList = Announcement & { class: Class }
 
-                <div className='flex flex-col'>
-                    <h3 className='font-semibold'>{item.title}</h3>
-                </div>
-            </td>
-            <td className="hidden sm:table-cell text-xs">{item.class}</td>
-            <td className="hidden sm:table-cell text-xs">{item.date}</td>
-            <td>
-                <div className='flex items-center gap-2'>
-                    {
-                        role === 'admin' && (
-                            <>
-                                <FormModal
-                                    table='announcement'
-                                    type='update'
-                                    data={item}
-                                />
-                                <FormModal
-                                    table='announcement'
-                                    type='delete'
-                                    id={item.id}
-                                />
-                            </>
+const renderRow = (item: AnnouncementList) => (
+    <tr
+        key={item.id}
+        className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-mPurpleLight'
+    >
+        <td className='flex items-center gap-4 p-4'>
 
-                        )
-                    }
-                </div>
-            </td>
-        </tr>
-    )
+            <div className='flex flex-col'>
+                <h3 className='font-semibold'>{item.title}</h3>
+            </div>
+        </td>
+        <td className="hidden sm:table-cell text-xs">{item.class.name}</td>
+        <td className="hidden sm:table-cell text-xs">
+            {new Intl.DateTimeFormat('en-US').format(item.date)}
+        </td>
+        <td>
+            <div className='flex items-center gap-2'>
+                {
+                    role === 'admin' && (
+                        <>
+                            <FormModal
+                                table='announcement'
+                                type='update'
+                                data={item}
+                            />
+                            <FormModal
+                                table='announcement'
+                                type='delete'
+                                id={item.id}
+                            />
+                        </>
+
+                    )
+                }
+            </div>
+        </td>
+    </tr>
+)
+const AnnouncementsListPage = async ({ searchParams }: {
+    searchParams: { [key: string]: string | undefined }
+}) => {
+    const { page, ...queryParams } = searchParams
+    const p = page ? parseInt(page) : 1
+    const filter: Prisma.AnnouncementWhereInput = {}
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value != undefined) {
+                switch (key) {
+                    case "search":
+                        filter.OR = [
+                            { title: { contains: value, mode: 'insensitive' } },
+                            { class: { name: { contains: value, mode: 'insensitive' } } }
+                        ]
+                        break
+                    default:
+                        break
+                }
+            }
+        }
+    }
+    const [data, count] = await prisma.$transaction([
+        prisma.announcement.findMany({
+            where: filter,
+            include: {
+                class: { select: { name: true } }
+            },
+            take: ITEMS_PER_PAGE,
+            skip: ITEMS_PER_PAGE * (p - 1)
+        }),
+        prisma.announcement.count({
+            where: filter,
+        })
+    ])
     return (
         <div className='bg-white rounded-md p-4 m-4 mt-0'>
             {/* TOP */}
@@ -108,10 +143,13 @@ const AnnouncementsListPage = () => {
             <Table
                 columns={columns}
                 renderRow={renderRow}
-                data={announcementsData}
+                data={data}
             />
             {/* PAGINATION */}
-            <Pagination />
+            <Pagination
+                page={p}
+                count={count}
+            />
         </div>
     )
 }
