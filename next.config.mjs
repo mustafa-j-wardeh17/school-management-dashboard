319b6cd93fc5275995632588f13f18639c88fccc
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-03 13:16:17 +0300

    add cloudinary hostname config to next.config.mjs to accept showing cloudinary images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ import { hostname } from 'os';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{hostname:"images.pexels.com"}]
+        remotePatterns:[
+            {hostname:"images.pexels.com"},
+            {hostname:"res.cloudinary.com"},
+
+        ]
     }
 };
 
--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -1,209 +1,216 @@
-'use client'
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import InputField from '../InputField';
-import Image from 'next/image';
-import { teacherSchema, TeacherSchema } from '@/lib/formValidationSchema';
-import { createTeacher, updateTeacher } from '@/lib/actions';
-import { useFormState } from 'react-dom';
-import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
-import { CldUploadWidget } from 'next-cloudinary';
+"use client";
 
-const TeacherForm = ({ setOpen, type, data, relatedData }: {
-    setOpen: Dispatch<SetStateAction<boolean>>,
-    relatedData?: any;
-    type: 'create' | 'update';
-    data?: any
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import Image from "next/image";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+import { createTeacher, updateTeacher } from "@/lib/actions";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { CldUploadWidget } from "next-cloudinary";
+import { teacherSchema, TeacherSchema } from "@/lib/formValidationSchema";
+
+const TeacherForm = ({
+  type,
+  data,
+  setOpen,
+  relatedData,
+}: {
+  type: "create" | "update";
+  data?: any;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  relatedData?: any;
 }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TeacherSchema>({
+    resolver: zodResolver(teacherSchema),
+  });
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<TeacherSchema>({
-        resolver: zodResolver(teacherSchema),
-    });
+  const [img, setImg] = useState<any>();
 
-    const [img, setImg] = useState<any>()
+  const [state, formAction] = useFormState(
+    type === "create" ? createTeacher : updateTeacher,
+    {
+      success: false,
+      error: false,
+    }
+  );
 
-    // use formstate 
-    const [state, formAction] = useFormState(type === 'create'
-        ? createTeacher
-        : updateTeacher
-        , {
-            success: false, error: false
-        }
-    )
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    formAction({ ...data, img: img?.secure_url });
+  });
 
-    const onSubmit = handleSubmit((data) => {
-        formAction({ ...data, img: img?.secureUrl })
-    })
+  const router = useRouter();
 
-    const router = useRouter()
-    useEffect(() => {
-        if (state.success) {
-            toast(`Teacher has been ${type === "create" ? "created" : "updated"}!`);
-            setOpen(false);
-            router.refresh();
-        }
-    }, [state, router, type, setOpen]);
+  useEffect(() => {
+    if (state.success) {
+      toast(`Teacher has been ${type === "create" ? "created" : "updated"}!`);
+      setOpen(false);
+      router.refresh();
+    }
+  }, [state, router, type, setOpen]);
 
-    const { subjects } = relatedData;
-    return (
-        <form
-            onSubmit={onSubmit}
-            className='flex flex-col gap-8'
+  const { subjects } = relatedData;
+
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">
+        {type === "create" ? "Create a new teacher" : "Update the teacher"}
+      </h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentication Information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="Username"
+          name="username"
+          defaultValue={data?.username}
+          register={register}
+          error={errors?.username}
+        />
+        <InputField
+          label="Email"
+          name="email"
+          defaultValue={data?.email}
+          register={register}
+          error={errors?.email}
+        />
+        <InputField
+          label="Password"
+          name="password"
+          type="password"
+          defaultValue={data?.password}
+          register={register}
+          error={errors?.password}
+        />
+      </div>
+      <span className="text-xs text-gray-400 font-medium">
+        Personal Information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="First Name"
+          name="name"
+          defaultValue={data?.name}
+          register={register}
+          error={errors.name}
+        />
+        <InputField
+          label="Last Name"
+          name="surname"
+          defaultValue={data?.surname}
+          register={register}
+          error={errors.surname}
+        />
+        <InputField
+          label="Phone"
+          name="phone"
+          defaultValue={data?.phone}
+          register={register}
+          error={errors.phone}
+        />
+        <InputField
+          label="Address"
+          name="address"
+          defaultValue={data?.address}
+          register={register}
+          error={errors.address}
+        />
+        <InputField
+          label="Blood Type"
+          name="bloodType"
+          defaultValue={data?.bloodType}
+          register={register}
+          error={errors.bloodType}
+        />
+        <InputField
+          label="Birthday"
+          name="birthday"
+          defaultValue={data?.birthday.toISOString().split("T")[0]}
+          register={register}
+          error={errors.birthday}
+          type="date"
+        />
+        {data && (
+          <InputField
+            label="Id"
+            name="id"
+            defaultValue={data?.id}
+            register={register}
+            error={errors?.id}
+            hidden
+          />
+        )}
+        <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gray-500">Sex</label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("sex")}
+            defaultValue={data?.sex}
+          >
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+          </select>
+          {errors.sex?.message && (
+            <p className="text-xs text-red-400">
+              {errors.sex.message.toString()}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gray-500">Subjects</label>
+          <select
+            multiple
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("subjects")}
+            defaultValue={data?.subjects}
+          >
+            {subjects.map((subject: { id: number; name: string }) => (
+              <option value={subject.id} key={subject.id}>
+                {subject.name}
+              </option>
+            ))}
+          </select>
+          {errors.subjects?.message && (
+            <p className="text-xs text-red-400">
+              {errors.subjects.message.toString()}
+            </p>
+          )}
+        </div>
+        <CldUploadWidget
+          uploadPreset="school"
+          onSuccess={(result, { widget }) => {
+            setImg(result.info);
+            widget.close();
+          }}
         >
-            <h1 className='text-xl font-semibold'>{type === 'create' ? 'Create a new teacher' : 'Update the teacher'}</h1>
-            <span className='text-xs text-gray-400 font-medium'> Authentication Information</span>
-            <div className='flex flex-wrap gap-4 justify-between'>
+          {({ open }) => {
+            return (
+              <div
+                className="text-xs text-gray-500 flex items-center gap-2 cursor-pointer"
+                onClick={() => open()}
+              >
+                <Image src="/upload.png" alt="" width={28} height={28} />
+                <span>Upload a photo</span>
+              </div>
+            );
+          }}
+        </CldUploadWidget>
+      </div>
+      {state.error && (
+        <span className="text-red-500">Something went wrong!</span>
+      )}
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
 
-                <InputField
-                    label='username'
-                    name='username'
-                    defaultValue={data?.username}
-                    register={register}
-                    error={errors.username}
-                />
-                <InputField
-                    label='Email'
-                    name='email'
-                    type='email'
-                    defaultValue={data?.email}
-                    register={register}
-                    error={errors.email}
-                />
-                <InputField
-                    label='Password'
-                    name='password'
-                    type='password'
-                    defaultValue={data?.password}
-                    register={register}
-                    error={errors.password}
-                />
-            </div>
-
-            <span className='text-xs text-gray-400 font-medium'> Personal Information</span>
-            <div className='flex flex-wrap gap-4 justify-between'>
-                <InputField
-                    label='First Name'
-                    name='name'
-                    defaultValue={data?.name}
-                    register={register}
-                    error={errors.name}
-                />
-                <InputField
-                    label='Last Name'
-                    name='surname'
-                    defaultValue={data?.surname}
-                    register={register}
-                    error={errors.surname}
-                />
-                <InputField
-                    label='Phone'
-                    name='phone'
-                    defaultValue={data?.phone}
-                    register={register}
-                    error={errors.phone}
-                />
-                <InputField
-                    label='Address'
-                    name='address'
-                    defaultValue={data?.address}
-                    register={register}
-                    error={errors.address}
-                />
-                <InputField
-                    label='Blood Type'
-                    name='bloodType'
-                    defaultValue={data?.bloodType}
-                    register={register}
-                    error={errors.bloodType}
-                />
-                <InputField
-                    label='Blood Type'
-                    name='bloodType'
-                    defaultValue={data?.bloodType}
-                    register={register}
-                    error={errors.bloodType}
-                    type='date'
-                />
-                <div className='flex flex-col gap-2 w-full md:w-1/4'>
-                    <label className='text-xs text-gray-500'>Sex</label>
-                    <select
-                        defaultValue={data?.sex}
-                        {...register('sex')}
-                        className='ring-[1.5px] ring-gray-100 p-2 rounded-md text-sm w-full'
-                    >
-                        <option value="MALE">Male</option>
-                        <option value="FEMALE">Female</option>
-                    </select>
-                    {
-                        errors.sex?.message &&
-                        <p className='text-red-400 text-xs'>
-                            {errors.sex?.message.toString()}
-                        </p>
-                    }
-                </div>
-                <div className="flex flex-col gap-2 w-full md:w-1/4">
-                    <label className="text-xs text-gray-500">Subjects</label>
-                    <select
-                        multiple
-                        className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
-                        {...register("subjects")}
-                        defaultValue={data?.subjects}
-                    >
-                        {subjects.map((subject: { id: number; name: string }) => (
-                            <option value={subject.id} key={subject.id}>
-                                {subject.name}
-                            </option>
-                        ))}
-                    </select>
-                    {errors.subjects?.message && (
-                        <p className="text-xs text-red-400">
-                            {errors.subjects.message.toString()}
-                        </p>
-                    )}
-                </div>
-
-                <CldUploadWidget
-                    uploadPreset="school"
-                    onSuccess={(res, { widget }) => {
-                        setImg(res.info);
-                        widget.close()
-                    }}
-                >
-                    {({ open }) => {
-                        return (
-                            <div
-                                className='text-xs cursor-pointer text-gray-500 flex items-center gap-2 '
-                                onClick={() => open()}                            >
-                                <Image
-                                    src={'/upload.png'}
-                                    alt='upload image'
-                                    width={28}
-                                    height={28}
-                                />
-                                <span>Upload a photo</span>
-                            </div>
-                        );
-                    }}
-                </CldUploadWidget>
-
-
-            </div>
-            {
-                state.error && (
-                    <span className='text-red-500 text-xs'>
-                        Something went wrong!
-                    </span>
-                )
-            }
-            <button className='bg-blue-400 text-white p-2 rounded-md'>{type === 'create' ? 'Create' : 'Update'}</button>
-        </form>
-    )
-}
-
-export default TeacherForm
+export default TeacherForm;
